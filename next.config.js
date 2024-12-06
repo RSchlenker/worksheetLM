82d980912bf1d195adb9a1961fc4fcf2e7a3cc54
Robin Schlenker <robin.schlenker@codecentric.de>
2024-12-06 09:36:51 +0100

    Allow netlify domain to call server actions

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@react-pdf/renderer'],
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        'worksheetlm.netlify.app',
+        'ab.schlenker.io',
+      ],
+    },
   },
 }
+
+module.exports = nextConfig
